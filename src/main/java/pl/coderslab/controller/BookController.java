48cdfd9e903606c8d316693e263511495f9528ca
pablo <pw.supel@gmail.com>
2022-09-08 23:47:07 +0200

    refactor

--- src/main/java/pl/coderslab/controller/BookController.java
+++ src/main/java/pl/coderslab/controller/BookController.java
@@ -3,7 +3,10 @@ package pl.coderslab.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import pl.coderslab.entity.Book;
 import pl.coderslab.service.BookService;
 
--- src/main/java/pl/coderslab/entity/Book.java
+++ src/main/java/pl/coderslab/entity/Book.java
@@ -1,6 +1,10 @@
 package pl.coderslab.entity;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "books")
